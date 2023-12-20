1963258968da76d8e0b38a2bd2560eaebf1e3506
lehoainam3902 <lehoainam1233902@gmail.com>
2023-12-20 14:09:05 +0700

    update

--- config/services.php
+++ config/services.php
@@ -33,6 +33,6 @@ return [
     'facebook' => [
         'client_id' => env('FACEBOOK_APP_ID'),
         'client_secret' => env('FACEBOOK_APP_SECRET'),
-        'redirect' => '/auth/facebook/callback',
+        'redirect' => route('home').'/auth/facebook/callback',
     ],
 ];
